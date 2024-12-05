ec06a2b769f6ca21922365424001aa4bf8c4c531
picania <mangustspam@gmail.com>
2024-12-05 23:27:27 +0700

    Решение задачи two sum за линейное время

--- src/grind75/easy.rs
+++ src/grind75/easy.rs
@@ -7,16 +7,18 @@ pub mod two_sum {
 
     impl Solution {
         pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+            let mut map = std::collections::HashMap::<i32, usize>::new();
             let mut first = 0_usize;
             let mut second = 0_usize;
+
             for (i, v) in nums.iter().enumerate() {
                 let remain = target - *v;
-                for (k, v) in nums.iter().enumerate().skip(i + 1) {
-                    if remain == *v {
-                        first = i;
-                        second = k;
-                        break;
-                    }
+
+                if let Some(index) = map.get(&remain) {
+                    first = i;
+                    second = *index;
+                } else {
+                    map.insert(*v, i);
                 }
             }
 
@@ -30,19 +32,18 @@ mod tests {
 
     use super::two_sum::Solution;
 
-    fn check(nums: Vec<i32>, target: i32, indecies: Vec<i32>) {
+    fn check(nums: Vec<i32>, target: i32) {
         let res = Solution::two_sum(nums.clone(), target);
 
         let err_msg = format!("with input nums:{:?} and taget:{}", nums, target);
         assert_eq!(target, &nums[res[0] as usize] + &nums[res[1] as usize], "{}", &err_msg);
-        assert_eq!(indecies[0], res[0], "{}", &err_msg);
-        assert_eq!(indecies[1], res[1], "{}", &err_msg);
+        assert!(res[0] != res[1], "{}", &err_msg);
     }
 
     #[test]
     fn two_sum() {
-        check(vec![2, 7, 11, 15], 9, vec![0, 1]);
-        check(vec![3, 2, 4], 6, vec![1, 2]);
-        check(vec![3, 3], 6, vec![0, 1]);
+        check(vec![2, 7, 11, 15], 9);
+        check(vec![3, 2, 4], 6);
+        check(vec![3, 3], 6);
     }
 }
