6e9a51aaa09a935ba1e1d63b54bf3f6b3930ec1e
picania <mangustspam@gmail.com>
2024-12-06 00:18:28 +0700

    Решение задачи Valid Parentheses

--- src/grind75/easy.rs
+++ src/grind75/easy.rs
@@ -1,8 +1,8 @@
 #![allow(dead_code)]
 
-/// An implementation of [two sum](https://leetcode.com/problems/two-sum/description/) problem.
+/// An implementation of [Two Sum](https://leetcode.com/problems/two-sum/description/) problem.
 pub mod two_sum {
-    
+
     pub struct Solution;
 
     impl Solution {
@@ -27,16 +27,61 @@ pub mod two_sum {
     }
 }
 
+/// An implementation of [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/description/) problem.
+pub mod valid_parentheses {
+
+    pub struct Solution;
+
+    impl Solution {
+        pub fn is_valid(s: String) -> bool {
+            let mut stack = Vec::<char>::new();
+            for c in s.chars() {
+                match c {
+                    ')' => {
+                        if stack.last().is_some_and(|c| *c == '(') {
+                            stack.pop();
+                        } else {
+                            stack.push(c);
+                        }
+                    }
+                    ']' => {
+                        if stack.last().is_some_and(|c| *c == '[') {
+                            stack.pop();
+                        } else {
+                            stack.push(c);
+                        }
+                    }
+                    '}' => {
+                        if stack.last().is_some_and(|c| *c == '{') {
+                            stack.pop();
+                        } else {
+                            stack.push(c);
+                        }
+                    }
+                    _ => stack.push(c),
+                }
+            }
+
+            stack.is_empty()
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
 
-    use super::two_sum::Solution;
-
     fn check(nums: Vec<i32>, target: i32) {
+        use super::two_sum::Solution;
+
         let res = Solution::two_sum(nums.clone(), target);
 
         let err_msg = format!("with input nums:{:?} and taget:{}", nums, target);
-        assert_eq!(target, &nums[res[0] as usize] + &nums[res[1] as usize], "{}", &err_msg);
+        assert_eq!(
+            target,
+            &nums[res[0] as usize] + &nums[res[1] as usize],
+            "{}",
+            &err_msg
+        );
         assert!(res[0] != res[1], "{}", &err_msg);
     }
 
@@ -45,5 +90,23 @@ mod tests {
         check(vec![2, 7, 11, 15], 9);
         check(vec![3, 2, 4], 6);
         check(vec![3, 3], 6);
+    }
+
+    #[test]
+    fn valid_parentheses() {
+        use super::valid_parentheses::Solution;
+
+        assert!(Solution::is_valid("[]".into()));
+        assert!(Solution::is_valid("()".into()));
+        assert!(Solution::is_valid("{}".into()));
+
+        assert!(Solution::is_valid("()[]{}".into()));
+        assert!(Solution::is_valid("({[]})".into()));
+
+        assert!(!Solution::is_valid("(]".into()));
+        assert!(!Solution::is_valid("]".into()));
+        assert!(!Solution::is_valid("]()".into()));
+        assert!(!Solution::is_valid("[])".into()));
+        assert!(!Solution::is_valid("(])".into()));
     }
 }
