1da9bd19df8f4b07c0ed439795fc004835559715
picania <mangustspam@gmail.com>
2025-10-16 12:49:41 +0700

    Решена задача 15 3Sum Решение не очень оптимальное. Имеет квадратичную сложность и еле-еле пролезает по времени.

--- src/two_pointers.rs
+++ src/two_pointers.rs
@@ -58,3 +58,88 @@ mod p125 {
         }
     }
 }
+
+// [15. 3Sum](https://leetcode.com/problems/3sum)
+mod p15 {
+
+    pub struct Solution;
+
+    impl Solution {
+        pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+            let mut result = vec![];
+            let mut cache = std::collections::HashMap::<i32, Vec<(i32, i32)>>::new();
+
+            for i in 0..nums.len() {
+                let first = nums[i];
+                let remain = 0 - first;
+
+                let two_sum = if let Some(ts) = cache.get(&first) {
+                    ts.clone()
+                } else {
+                    let ts = Self::two_sum(&nums[i + 1..], remain);
+
+                    cache.insert(first, ts.clone());
+                    ts
+                };
+
+                for &(second, third) in &two_sum {
+                    let mut r = vec![first, second, third];
+
+                    r.sort();
+                    result.push(r);
+                }
+            }
+
+            result.sort();
+            result.dedup();
+
+            result
+        }
+
+        pub fn two_sum(nums: &[i32], sum: i32) -> Vec<(i32, i32)> {
+            let mut result = vec![];
+            let mut cache = std::collections::HashSet::<i32>::new();
+
+            for i in 0..nums.len() {
+                let first = nums[i];
+                let remain = sum - first;
+
+                if let Some(&second) = cache.get(&remain) {
+                    result.push((first, second));
+                } else {
+                    cache.insert(first);
+                }
+            }
+
+            result
+        }
+    }
+
+    #[cfg(test)]
+    mod test {
+        use crate::two_pointers::p15::Solution;
+
+        #[test]
+        fn example_i() {
+            let nums = vec![-1, 0, 1, 2, -1, -4];
+            let expected = vec![vec![-1, -1, 2], vec![-1, 0, 1]];
+
+            assert_eq!(expected, Solution::three_sum(nums));
+        }
+
+        #[test]
+        fn example_ii() {
+            let nums = vec![0, 1, 1];
+
+            assert!(Solution::three_sum(nums).is_empty());
+        }
+
+        #[test]
+        fn example_iii() {
+            let nums = vec![0, 0, 0];
+            let expected = vec![vec![0, 0, 0]];
+
+            assert_eq!(expected, Solution::three_sum(nums));
+        }
+    }
+}
